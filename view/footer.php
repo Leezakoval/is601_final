6a82304a244bc1c6631d497a8064a2f2dd25718a
Leeza Koval <yk247@njit.edu>
2017-08-04 07:38:33 -0400

    added view folder

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Leeza's Todo List.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My ToDo list</title>
+    <link rel="stylesheet" type="text/css"
+          href="/book_apps/ch05_guitar_shop/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Todo List</h1></header>

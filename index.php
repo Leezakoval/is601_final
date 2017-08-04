866976d719e327f4b8f0e78ee10823f67a669abf
Leeza Koval <yk247@njit.edu>
2017-08-04 08:22:11 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'view/header.php'; ?>
+<div id="main">
+    <h1 class="top">Home Page</h1>
+    <p><a href="product_manager">Go to your Task List</a></p>
+    <p><a href="product_catalog">Remove this section//Product Catalog</a></p>
+</div>
+<?php include 'view/footer.php'; ?>

3250fcf3cf0241a6c433ca1a076967ebc27a3659
Leeza Koval <yk247@njit.edu>
2017-08-04 08:27:58 -0400

    updated header

--- view/header.php
+++ view/header.php
@@ -3,8 +3,7 @@
 <!-- the head section -->
 <head>
     <title>My ToDo list</title>
-    <link rel="stylesheet" type="text/css"
-          href="/book_apps/ch05_guitar_shop/main.css">
+    <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
